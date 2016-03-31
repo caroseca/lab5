2fe7fd8a99087a44191d2d9b350704e62d1b6ae0
Cara Canady <cara.canady@gmail.com>
2016-03-30 20:39:00 -0400

    This is the first stab at a shopping list

---
+++ shoppingList.js
@@ -0,0 +1,27 @@
+var myList = [
+	{
+		name: "banana",
+		price: .5
+	},
+		name: "apple",
+		price: 1
+
+]
+	name: ["banana", "apple", "dintyMooreBeefStew"],
+	price: [0,1,2.25]
+}
+
+var printList = function(el) {
+	console.log(el);
+}
+
+// myList.forEach.name(printList);
+
+var total = myList.price[0] += myList.price[1] += myList.price[2];
+
+console.log("Total is" + total);
+
+
+
+
+myList.name.forEach(printList);
